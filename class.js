2ef10296ef18e29d4555df3fde5f8eb023915a9d
ashot-24 <ashot.k.y@tumo.org>
2020-01-25 19:19:09 +0400

    lesson 4

---
+++ class.js
@@ -0,0 +1,9 @@
+module.exports = class xot{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+    }
+    mul() {
+        console.log("bazmanal")
+    }
+}
--- index.js
+++ index.js
@@ -20,15 +20,38 @@
 //    console.log("Example is running on port 3000");
 // });
 
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/", function(req, res){
-   res.redirect("index.html");
-});
+// app.get("/", function(req, res){
+//    res.redirect("index.html");
+// });
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+
+
+// var grass = require("./class")
+// var norxot = new grass(12,42)
+
+// norxot.mul();
+
+var fs = require("fs");
+
+// fs.readFileSync("hello.txt", "utf8", 
+//    console.log("fs.writeFile ended")
+// )
+// console.log("fs.writeFile");
+var obj = {
+   "first_name ":"Ashot",
+   "last_name":"Khachatryan",
+   "age":15,
+   "st":true
+}
+var text = JSON.stringify(obj);
+
+fs.writeFileSync("myobj.json" , text);
